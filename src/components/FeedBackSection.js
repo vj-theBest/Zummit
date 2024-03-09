016bcf342b2ef9401f80055633f95d73eba0acd2
Vaibhav-Thalanki <vaibhav.thalanki@gmail.com>
2024-03-09 15:42:13 +0530

    added contact and feedback
    by ayush

--- src/components/FeedBackSection.js
+++ src/components/FeedBackSection.js
@@ -1,76 +1,76 @@
-import React from "react";
+import React from 'react'
+
+const FeedbackSection = () => {
+  return (
+    <div>
+        <div className="w-3/5 mx-[10vw]">
+            <h2 className="font-normal text-2xl mt-3 mb-6">Clients Feedback</h2>
+            <p className="text-lg">These quotes represent a few of many positive reviews that we have recieved for therapists. 
+                If you would like to review your therapists, please send your review to
+            </p>
+        </div>
+        <div className="flex flex-wrap gap-6 py-12 pl-12 pr-16">
+          {
+            feedback.map((feedback) => {
+              return (
+                <div className="bg-[rgb(235,247,255)] min-w-[260px] max-w-xs grid gap-[7px] shadow-[rgba(0,0,0,0.24)_0px_3px_8px] p-3.5 rounded-[5px]" key={feedback.id}>
+                  <p className="font-normal">{feedback.message}</p>
+                  <div className="self-end text-cyan-600 text-sm">
+                    {feedback.author} | {feedback.date}
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+    </div>
+  )
+}
+
+export default FeedbackSection
 
 const feedback = [
   {
     id: "1",
     message: "Hands down, One of the best",
     author: "Laura Craft",
-    date: "20-02-20",
+    date: "20-02-20"
   },
   {
     id: "2",
     message: "Hands down, One of the best theprapist I have had",
     author: "Laura Craft",
-    date: "20-02-20",
+    date: "20-02-20"
   },
   {
     id: "3",
     message: "Hands down, One of the best theprapist I have had",
     author: "Laura Craft",
-    date: "20-02-20",
+    date: "20-02-20"
   },
   {
     id: "4",
     message: "Hands down, One of the best theprapist I have had",
     author: "Laura Craft",
-    date: "20-02-20",
+    date: "20-02-20"
   },
   {
     id: "5",
     message: "Hands down, One of the best theprapist I have had",
     author: "Laura Craft",
-    date: "20-02-20",
+    date: "20-02-20"
   },
   {
     id: "6",
-    message:
-      "Hands down, One of the best theprapist I ever have had, highly recommended",
+    message: "Hands down, One of the best theprapist I ever have had, highly recommended",
     author: "Laura Craft",
-    date: "20-02-20",
+    date: "20-02-20"
   },
   {
     id: "7",
     message: "Hands down, One of the best theprapist I have had",
     author: "Laura Craft",
-    date: "20-02-20",
+    date: "20-02-20"
   },
-];
-
-const FeedbackSection = () => {
-  return (
-    <div className="bg-skin py-10">
-      <div className="Feedback_header">
-        <h2 className="font-normal text-2xl pb-8">Clients Feedback</h2>
-        <p className="text-lg">
-          These quotes represent a few of many positive reviews that we have
-          recieved for therapists. If you would like to review your therapists,
-          please send your review to
-        </p>
-      </div>
-      <div className="flex flex-wrap gap-8 py-12 pl-8 ">
-        {feedback.map((feedback) => {
-          return (
-            <div className="feedback_card " key={feedback.id}>
-              <p className="font-normal">{feedback.message}</p>
-              <div className="self-end text-cyan-600 text-sm">
-                {feedback.author} | {feedback.date}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
-
-export default FeedbackSection;
+  
+]
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import Hero from './Hero';
-import Card from './Card';
-import Carosel from './Carosel'; 
-
-
+import Hero from "./Hero";
+import Card from "./Card";
+import Carosel from "./Carosel";
+import ContactSection from "./ContactSection";
+import FeedbackSection from "./FeedBackSection";
 
 const Home = () => {
   return (
     <div>
+      <Hero />
 
-     
-      <Hero/>
-      
-      <Card/>
-      <Carosel/>
-      {/* <FeedbackSection/>
-      <ContactSection/> */}
-    
-
+      <Card />
+      <Carosel />
+      <FeedbackSection />
+      <ContactSection />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
