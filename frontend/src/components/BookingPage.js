fe8b30cb738ab6d30d22052413c76e2aeb0e1e21
Chandan Kumal <85206406+chandan24042001s@users.noreply.github.com>
2024-04-07 17:30:30 +0000

    booking page

--- frontend/src/components/BookingPage.js
+++ frontend/src/components/BookingPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BookingPage = () => {
   return (
     <div className='h-[80vh] justify-center items-center text-5xl font-bold' >
-      <h3>Abhi kee liye apka Swagat hey</h3>
+      <h3>This will be a booking Page </h3>
       <h3>Releasing Soon ........</h3>
     </div>
   )
