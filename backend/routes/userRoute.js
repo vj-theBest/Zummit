d0ecb094a5243ba86299dafb5e582ecfad9b936b
chandan <hb7102849@gmail.com>
2024-04-25 07:47:38 +0530

    aab chalo

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const {registerUser, loginUser, logOut, getUser}=require("../controllers/userController");
 const { protect } = require("../middleware/authMiddleware");
 
-
+//removed
 router.post("/register",registerUser);
 router.post("/login",loginUser)
 router.get("/logout",logOut)
