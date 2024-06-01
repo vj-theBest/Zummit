a758e89c5c565ea3f2905e3e0e667a8f10d55656
Akib-Vijapura <akibv93@gmail.com>
2024-06-01 22:25:51 +0530

    Admin : resolve admin login bug

--- backend/controllers/Admin/adminController/register_Login.js
+++ backend/controllers/Admin/adminController/register_Login.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const AdminLoginRegister = require("../../../models/Admin/AdminRegisterLogin/adminModel");
 
-
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
@@ -11,85 +10,78 @@ const generateToken = (id) => {
 };
 
 const registerAdmin = asyncHandler(async (req, res) => {
-  let isValid = false;
-  let msg = '';
   const { name, input, password, role } = req.body;
 
-  if (password.length < 6) {
-    return res.status(400).json({ success: false, msg: "Password must be at least 6 characters long." });
+  if (!name || !input || !password || !role) {
+    return res.status(400).json({ message: "Please provide all fields" });
   }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(input)) {
-    return res.status(400).json({ success: false, msg: "Please enter a valid email address." });
-  }
-
-  const adminExists = await AdminLoginRegister.findOne({ input });
-  if (adminExists) {
-    return res.status(400).json({ success: false, msg: "Admin already registered." });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const admin = await AdminLoginRegister.create({
-    name,
-    input,
-    password: hashedPassword,
-    role,
-  });
-
-  if (admin) {
-    const { _id, input, role } = admin;
-    const token = generateToken(_id);
-    res.cookie("token", token, {
-      path: "/",
-      httpOnly: true,
-      expires: new Date(Date.now() + 1000 * 86400),
-    });
-    res.status(201).json({
-      success: true,
-      data: _id,
+    const admin = new AdminLoginRegister({
       name,
       input,
       role,
-      token,
-      message: "Admin registered successfully.",
+      password: hashedPassword,
     });
-  } else {
-    res.status(400).json({ error: "Invalid admin data" });
+
+    await admin.save();
+
+     if (admin) {
+       const { _id, input, role } = admin;
+       const token = generateToken(_id);
+       res.cookie("token", token, {
+         path: "/",
+         httpOnly: true,
+         expires: new Date(Date.now() + 1000 * 86400),
+       });
+       res.status(201).json({
+         success: true,
+         admin,
+         message: "Admin registered successfully.",
+         token,
+       });
+     } else {
+       res.status(400).json({ error: "Invalid admin data" });
+     }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ message: "Server Error" });
   }
 });
-
 
 const loginAdmin = asyncHandler(async (req, res) => {
   const { input, password } = req.body;
 
-  if (!input ||!password) {
-    return res.status(400).json({ success: false, msg: "Please provide email and password." });
+  if (!input || !password) {
+    return res.status(400).json({ message: "Please provide all fields" });
   }
 
-  const admin = await AdminLoginRegister.findOne({ input });
-  if (!admin) {
-    return res.status(400).json({ success: false, msg: "No admin found with this email." });
+  try {
+    const admin = await AdminLoginRegister.findOne({ input }).select(
+      "+password"
+    );
+
+    if (!admin) {
+      return res.status(404).json({ message: "User Not Found!" });
+    }
+
+    const isMatch = await admin.matchPasswords(password);
+
+    if (admin && isMatch) {
+      res.status(200).json({
+        success: true,
+        admin,
+        token: generateToken(admin._id),
+      });
+    } else {
+      return res.status(400).json({ message: "Please check your password" });
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ message: "Server Error" });
   }
-
-  const passwordIsCorrect = await bcrypt.compare(password, admin.password);
-  
-  if (!passwordIsCorrect) {
-    return res.status(400).json({ success: false, msg: "Incorrect password." });
-  }
-
-  const token = generateToken(admin._id);
-  res.cookie("token", token, {
-    path: "/",
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000 * 86400),
-  });
-
-  res.status(200).json({
-    success: true,
-    message: "Admin logged in successfully.",
-    token,
-  });
 });
 
 module.exports = {
