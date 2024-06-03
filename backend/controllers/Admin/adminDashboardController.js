ddee9959fd3affbe82b0bea1c6dd22d3b2ef4462
nishaa12 <nishakrim40@gmail.com>
2024-06-03 23:23:17 +0530

    create post api for admindashboard

--- backend/controllers/Admin/adminDashboardController.js
+++ backend/controllers/Admin/adminDashboardController.js
@@ -46,8 +46,49 @@ const adminDashboard = asyncHandler(async (req, res) => {
       res.status(500).json({ message: "Server error" });
     }
 });
+
+const createadminDashboard = asyncHandler(async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { input, token, adminDashboardDetails } = req.body;
+
+  if (!input || !token || !adminDashboardDetails) {
+    return res.status(402).json({ message: "Please fill all fileds" });
+  }
+
+  try {
+    const admin = await AdminLoginRegister.findOne({ input }).select(
+      "-password"
+    )
+    if (!admin) {
+      return res.status(404).json({ message: "adminDashboard Details not found" });
+    }
+
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    if (JSON.stringify(decodedToken.id) !== JSON.stringify(admin._id)) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const newadminDashboardDetails = new adminDashboardDetails(adminDashboardDetails);
+    await adminDashboardDetails.save()
+
+    res.status(200).json({
+      success: true,
+      therapists: newadminDashboardDetails,
+      message: "adminDashboard created successfully"
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
     
 module.exports = {
-  adminDashboard,
+  adminDashboard,createadminDashboard
   
 };
